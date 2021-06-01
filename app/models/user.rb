059ca9f7b7c8fb8ce1922e57639d40516cb2e586
seina25 <replace421@gmail.com>
2021-06-01 16:09:07 +0000

    [Finish]bookers2-debug

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :books, dependent: :destroy
+  attachment :profile_image, destroy: false
+
+  validates :name, length: { in: 2..20 }, uniqueness: true, presence: true
+  validates :introduction, length: { maximum: 50 }
+end
